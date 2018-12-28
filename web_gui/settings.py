9dd3777dbf61bf4a4deeaaf1dc84c979b0aeadab
Rafał Lis <rafallis95@gmail.com>
2018-12-28 13:11:33 +0100

    suicide

--- web_gui/settings.py
+++ web_gui/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = 'D:/home/site/wwwroot'
